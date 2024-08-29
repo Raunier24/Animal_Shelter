85b2ccae06394e96420f0674593388d9f8411027
Raunier24 <peplisoft@gmail.com>
2024-08-29 11:29:14 +0200

    working on class Donation fixing jakarta.persistence.Id

--- src/main/java/com/project/Animal_Shelter/model/Donation.java
+++ src/main/java/com/project/Animal_Shelter/model/Donation.java
@@ -1,10 +1,11 @@
 package com.project.Animal_Shelter.model;
 
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 
 @Entity
--- src/main/java/com/project/Animal_Shelter/service/UserDetailsServiceImpl.java
+++ src/main/java/com/project/Animal_Shelter/service/UserDetailsServiceImpl.java
@@ -20,7 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
             .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con el nombre: " + username));
-        
+
         return UserDetailsImpl.build(user);
     }
 }
